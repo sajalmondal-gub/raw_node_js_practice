4edc791576454fd5b6a0501e2a706a238bfa0847
sajalmondal-gub <sajalmondal.gub@gmail.com>
2026-06-30 23:53:05 +0600

    Implement CORS support and add CRUD operations for todos in server

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const http = require("http");
+const { json } = require("stream/consumers");
 const { URL } = require("url");
 // const querystring = require("querystring");
+
+// In-memory data store (for demonstration)
+let todos = [
+  { id: 1, task: "Learn Node.js", completed: false },
+  { id: 2, task: "Build an API", completed: false },
+];
 
 const server = http.createServer((req, res) => {
   //  console.log('Request Headers:', req.headers);
@@ -23,37 +30,77 @@ const server = http.createServer((req, res) => {
 
   //  }))
 
-//   console.log(URL);
-//   const basedUrl = "http://" + req.headers.host + "/";
-//   console.log("basedUrl", basedUrl);
-//   const myUrl = new URL(req.url, basedUrl);
-//   console.log("myUrl", myUrl);
+  //   console.log(URL);
+  //   const basedUrl = "http://" + req.headers.host + "/";
+  //   console.log("basedUrl", basedUrl);
+  //   const myUrl = new URL(req.url, basedUrl);
+  //   console.log("myUrl", myUrl);
 
-//   const queryObject = {
-//     name: "John",
-//     age: 30,
-//     city: "New York",
-//   };
+  //   const queryObject = {
+  //     name: "John",
+  //     age: 30,
+  //     city: "New York",
+  //   };
 
-//   const queryStr = querystring.stringify(queryObject);
-//   console.log("Query String:", queryStr);
-//   res.writeHead(200, { "Content-Type": "application/json" });
-//   res.end(
-//     JSON.stringify({
-//       path: parsedUrl.pathname,
-//       params,
-//       exampleQueryString: queryStr,
-//     }),
-//   );
+  //   const queryStr = querystring.stringify(queryObject);
+  //   console.log("Query String:", queryStr);
+  //   res.writeHead(200, { "Content-Type": "application/json" });
+  //   res.end(
+  //     JSON.stringify({
+  //       path: parsedUrl.pathname,
+  //       params,
+  //       exampleQueryString: queryStr,
+  //     }),
+  //   );
 
+  const { method, url } = req;
+  const parsedUrl = new URL(url, `http://${req.headers.host}`);
+  const pathname = parsedUrl.pathname;
 
+  // Set CORS headers (for development)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS",
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-
-
+  if (method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+  if (method === "GET" && pathname === "/todos") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(todos));
+  } else if (method === "POST" && pathname === "/todos") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      try {
+        const newTodo = JSON.parse(body);
+        newTodo.id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+        todos.push(newTodo);
+        res.writeHead(201, { "content-type": "application/json" });
+        res.end(JSON.stringify(newTodo));
+      } catch (err) {
+        res.writeHead(400, { "content-type": "application/json" });
+        res.end(JSON.stringify({ error: "invalid json" }));
+      }
+    });
+  } else if (method === "PUT" && pathname.startsWith("/todos/")) {
+    const id = parseInt(pathname.split("/")[2]);
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+  }
 });
 
 const PORT = 3000;
 
-server.listen(PORT, "localhost", () => {
+server.listen(PORT, () => {
   console.log("server is running on port 3000");
 });
