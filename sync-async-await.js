6bc405bfb929d565cd781cdcf67f71bb4ad24ec9
sajalmondal-gub <sajalmondal.gub@gmail.com>
2026-07-01 17:14:25 +0600

    Comment out synchronous file read and enhance async file read logging

--- sync-async-await.js
+++ sync-async-await.js
@@ -2,7 +2,13 @@ const fs=require('fs');
 
 
 // sync
-const mydata=fs.readFileSync('data.txt','utf8');
-console.log(mydata);
+// const mydata=fs.readFileSync('data.txt','utf8');
+// console.log(mydata);
 
 //async file read
+
+fs.readFile('data.txt',"utf-8",(err,data)=>{
+    //  console.log(err);
+    console.log(data);
+});
+console.log('this is after async readfile log');
