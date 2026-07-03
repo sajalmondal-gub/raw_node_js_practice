afca02bd0ee896b516c1c10d476d979d3e302e90
sajalmondal-gub <sajalmondal.gub@gmail.com>
2026-07-03 11:37:46 +0600

    Add initial https.js file with commented-out code for future reference

---
+++ https.js
@@ -0,0 +1,5 @@
+import https from "https";
+
+// console.log('this is https',https);
+
+// for open ssl i mean cert.pem file create openssl req -new -x509 -key key.pem -out cert.pem -days 365 -nodes using git bash and cd /c/Users/sajal this direcoty from
